8b49d0177ecf1ac52fe14a132c11260babf29e54
Pawel Medon <sqlk@op.pl>
2019-03-23 10:22:46 +0000

    pierwszy commit

---
+++ src/main/java/com/sda/lessons/App.java
@@ -0,0 +1,20 @@
+package com.sda.lessons;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+        int tab01[] = new int [3];
+        tab01[0] = 1;
+        tab01[0] = 2;
+        tab01[0] = 3;
+
+        int tab02[] = {1,2,3};
+    }
+}
