5748beee36f7ce7c992e800946addf7e1ff6b92b
Pawel Medon <sqlk@op.pl>
2019-03-23 12:57:39 +0000

    added array printing

--- src/main/java/com/sda/lessons/App.java
+++ src/main/java/com/sda/lessons/App.java
@@ -16,5 +16,40 @@ public class App
         tab01[0] = 3;
 
         int tab02[] = {1,2,3};
+
+
+        int tab03[][] = new int [3][3];
+        tab03[0][0] = 1;
+        tab03[0][1] = 2;
+        tab03[0][2] = 3;
+        tab03[1][0] = 4;
+        tab03[1][1] = 5;
+        tab03[1][2] = 6;
+        tab03[2][0] = 7;
+        tab03[2][1] = 8;
+        tab03[2][2] = 9;
+
+        int tab04[][] = {
+                {1,2,3},
+                {4,5,6,4},
+                {7,8,9},
+                {4,5,3,6,7,1}
+        };
+
+
+        int tab5[][] = new int[3][];
+        tab5[0] = new int[8];
+        tab5[0] = new int[3];
+        tab5[0] = new int[5];
+
+        for (int i = 0; i < (tab04.length); i++) {
+            int[] innerTable = tab04[i];
+            int innerLength = innerTable.length;
+            for (int j =0; j < innerLength; j++){
+                System.out.print(innerTable[j] + ", ");
+            }
+            System.out.print("\n");
+        }
+
     }
 }
